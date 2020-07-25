9702c85f9e84a049cbe84916b74958d096956d7c
stealthcopter <matthewrollings@pm.me>
2020-07-25 14:22:18 +0100

    updated test output

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -9,10 +9,10 @@ GREEN="${C}[1;32m"
 Y="${C}[1;33m"
 NC="${C}[0m"
 
-printTest() { 
-    line='.......................................................'
-    NAME="Test #000:$2 "
-    printf "Test #%03d:%s %s " "$1" "$2" "${line:${#NAME}}"
+printTest() {
+  line='.......................................................'
+  NAME="Test #000:$2 "
+  printf "Test #%03d:%s %s " "$1" "$2" "${line:${#NAME}}"
 }
 
 printPass() { printf "%sPass%s\n" "${GREEN}" "$NC"; }
@@ -29,41 +29,44 @@ testNo=0
 exitCode=0
 index="results/index.md"
 
-echo -e "# Test Results\n" > $index
+echo -e "{% include tests.md %}" >$index
 
 # Run all tests found in this folder
 for filename in *.sh; do
 
-    if [ "$filename" = "run-all.sh" ]; then
-        continue
-    fi
+  if [ "$filename" = "run-all.sh" ]; then
+    continue
+  fi
 
-    testNo=$((testNo+1))
+  testNo=$((testNo + 1))
 
-    name=$(basename "$filename" .sh)
-    
-    printTest "$testNo" "$name"
-    
-    "./$filename" 2>/dev/null 1>&2
-    RESULT=$?
+  name=$(basename "$filename" .sh)
 
-    if [ $RESULT -eq 0 ]; then
-        printPass
-        echo "- [$name]($name.html) - PASS" >> $index
-    elif [ $RESULT -eq 1 ]; then 
-        printIgnore
-        echo "- [$name]($name.html) - IGNORED" >> $index
-    else
-        printFail
-        exitCode=1
-        echo "- [$name]($name.html) - FAILED" >> $index
-    fi
-    
-    # If possible generate html output for the logs
-    if [ -x "$(command -v aha)" ] & [ -f "results/$name.log" ]; then
-        aha -b -f "results/$name.log" > "results/$name.html"
-    fi
-    
+  printTest "$testNo" "$name"
+
+  "./$filename" 2>/dev/null 1>&2
+  RESULT=$?
+
+  if [ $RESULT -eq 0 ]; then
+    printPass
+    echo "- [$name]($name.html) - PASS" >>$index
+  elif [ $RESULT -eq 1 ]; then
+    printIgnore
+    echo "- [$name]($name.html) - IGNORED" >>$index
+  else
+    printFail
+    exitCode=1
+    echo "- [$name]($name.html) - FAILED" >>$index
+  fi
+
+  # If possible generate html output for the logs
+  if
+    [ -x "$(command -v aha)" ] &
+    [ -f "results/$name.log" ]
+  then
+    aha -b -f "results/$name.log" >"results/$name.html"
+  fi
+
 done
 
 exit $exitCode
